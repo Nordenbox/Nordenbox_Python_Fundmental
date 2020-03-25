92519ff2550c6aefcafc8d5268f62250b5013dea
Nordenbox <nordenbox@gmail.com>
2020-03-25 22:46:04 +0800

    update

---
+++ .history/getPrimies_Filter_20200325224508.py
@@ -0,0 +1,24 @@
+''' utf-8'''
+
+def find_primes(n):
+    """[使用 Filter 函数筛选质数]
+
+Returns:
+    [boolean] -- [false：不是质数。True：是质数]
+"""
+    if n < 2:
+        return False
+    else:
+
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        else:
+            return True
+        # 这个地方很关键，不能在 if 之后接 else，否则需要再写一轮循环。
+        # 只需要让 if 语句完全没有 return，进入 for-else，则必然是质数。
+
+
+get_primes = filter(find_primes, [i for i in range(1, 1001)]) # filter函数，只要满足寻找素数的函数返回值为 False，就在列表中剔除。
+a = list(get_primes)
+print(a, len(a))
--- .history/sortAlgorithm_20200325170417.py
+++ .history/sortAlgorithm_20200325170417.py
@@ -18,10 +18,12 @@ def my_sort(l):
     return sorted_list
 
 
-a = [random.randint(1, 1000) for i in range(1, 300)]
-print(a, '\n*******************************')
-time_start = time.time()
-print(my_sort(a)[-1])
-time_end = time.time()
-print(time_end-time_start, 'second')
+# a = [random.randint(1, 1000) for i in range(1, 300)]
+# print(a, '\n*******************************')
+# time_start = time.time()
+# print(my_sort(a)[-1])
+# time_end = time.time()
+# print(time_end-time_start, 'second')
 
+list_unsort = [[1, 2, 5, 1, 3, 1, 5, 2, 6, 1], [4, 5, 3, 4, 5, 2, 4, 1, 6, 4], [4, 3, 1, 5, 4, 4, 6, 5, 6, 6], [3, 1, 1, 3, 5, 3, 4, 2, 3, 3], [6, 3, 6, 6, 2, 5, 5, 5, 6, 2], [4, 4, 5, 3, 3, 1, 1, 1, 4, 4], [3, 4, 6, 2, 6, 2, 3, 2, 6, 6], [3, 1, 4, 6, 4, 4, 6, 1, 6, 6], [3, 3, 3, 5, 2, 3, 2, 5, 1, 2], [2, 3, 2, 5, 6, 2, 5, 5, 1, 6]]
+map(list_unsort,my_sort(list_unsort))
